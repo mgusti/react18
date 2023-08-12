f9ae73633629f42c776250e054364bc24b366053
Muhammad Gusti Kurniawan <mahkluk.astral@gmail.com>
2023-08-12 08:26:48 +0700

    Add files via upload

---
+++ src/Testing.js
@@ -0,0 +1,4 @@
+const testing = () => {
+  return <div>testing</div>
+}
+export default testing
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  )
+}
+
+const Person = () => <h2>john doe</h2>
+const Message = () => {
+  return <p>this is message</p>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<Greeting></Greeting>)
